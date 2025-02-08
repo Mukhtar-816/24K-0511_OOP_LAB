ee4d26c26d1901072a4ab767e683a4aa11ec6f01
Mukhtar Shaikh <115702231+Mukhtar-816@users.noreply.github.com>
2025-02-09 00:16:35 +0500

    Add files via upload

--- Lab_03/Q5.cpp
+++ Lab_03/Q5.cpp
@@ -1,45 +1,143 @@
 #include <iostream>
 using namespace std;
 
-
 class Book {
-    public :
-    string _BookName;
-    int _ISBNNumber;
-    string _AuthorName;
-    string _PublisherName;
+private:
+    string bookName;
+    string isbn;
+    string author;
+    string publisher;
 
-    void AddBook(){
-        cout << "Enter the BookName, ISBNNumber AuthorName, and PublisherName :" << endl;
-        cin >> _BookName >> _ISBNNumber >> _AuthorName >> _PublisherName;
+public:
+    Book(string bookName, string isbn, string author, string publisher) {
+        this->bookName = bookName;
+        this->isbn = isbn;
+        this->author = author;
+        this->publisher = publisher;
+    }
+
+    string getBookName() { return this->bookName; }
+    string getISBN() { return this->isbn; }
+    string getAuthor() { return this->author; }
+    string getPublisher() { return this->publisher; }
+
+    string getBookInfo() {
+        return "Book Name: " + this->bookName + "\nISBN: " + this->isbn + "\nAuthor: " + this->author + "\nPublisher: " + this->publisher;
+    }
+};
+
+int main() {
+    Book books[5] = {
+        Book("The Catcher in the Rye", "978-0-316-76948-0", "J.D. Salinger", "Little, Brown and Company"),
+        Book("1984", "978-0-452-28423-4", "George Orwell", "Secker & Warburg"),
+        Book("To Kill a Mockingbird", "978-0-06-112008-4", "Harper Lee", "J.B. Lippincott & Co."),
+        Book("The Great Gatsby", "978-0-7432-7356-5", "F. Scott Fitzgerald", "Charles Scribner's Sons"),
+        Book("Moby Dick", "978-0-14-243724-7", "Herman Melville", "Harper & Brothers")
     };
-    void RemoveBook(Book *book_arr){
-        int ID = 0;
-        for (int i = 0; i < 5; ++i){
-            cout << i + 1 << ". " << book_arr[i]._BookName << " " << book_arr[i]._ISBNNumber << " " << book_arr[i]._AuthorName << " " << book_arr[i]._PublisherName << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Book " << i + 1 << " Info:\n" << books[i].getBookInfo() << "\n\n";
     }
-    cout << endl << "Enter ID : ";
-    cin >> ID;
-};};
 
-
-
-int main(){
-    Book book_arr[5];
-    string options_arr[5] = {"Add Book.", "Remove Book", "Search Book by ISBN Number.", "See Book Info.", "Upadte Book."};
-    int current_option;
-
-    cout << "Choose from the optioms below : \n";
-    for (int i = 0; i < 5; i++){
-        cout << i + 1 << "." << options_arr[i] << endl;
-    }
-    cout << "Enter your option : ...." << endl;
-    cin >> current_option;
-
-    if (current_option - 1 == 0){
-        book_arr[0].AddBook();
-    }
-    else if (current_option - 1 == 1){
-        book_arr[1].RemoveBook(book_arr);
-    }
+    return 0;
 }
+
+
+
+
+
+
+// using namespace std;
+// #include <string.h>
+
+// class Book {
+//     public :
+//     string bookName;
+//     string ISBNNumber;
+//     string authorName;
+//     string publisherName;
+//     int index = 0;
+
+
+//     void AddBook(Book *book){
+//         cout << "Enter BookName, ISBN number, AuthorName, and PublisherName respectively :" << endl;
+//         cin >> book[index].bookName >> book[index].ISBNNumber >> book[index].authorName >> book[index].publisherName;
+//         index++;
+//         cout << "Book Added." << endl;
+//     }
+
+//     void RemoveBook(Book *book){
+//         string tempID; int found = 0;
+//         cout << "Enter Book ISBN Number to remove :" << endl;
+//         cin >> tempID;
+
+//         for (int i = 0; i < 5; ++i){
+//             if (tempID == book[i].ISBNNumber){
+//                 book[i].bookName = "";
+//                 book[i].ISBNNumber = "";
+//                 book[i].authorName = "";
+//                 book[i].publisherName = "";
+//                 found = 1;
+//                 cout << "Book Removed." << endl;
+//                 break;
+//             }
+//         }
+//         if (!found){
+//             cout << "Book not found." << endl;
+//         }
+//     };
+
+//     void SearchBook(Book *book){
+//         string tempID; int found = 0;
+//         cout << "Enter Book ISBN Number :" << endl;
+//         cin >> tempID;
+
+//         for (int i = 0; i < 5; ++i){
+//             if (tempID == book[i].ISBNNumber){
+//                 cout << "BookName :" << book[i].bookName << endl;
+//                 cout << "ISBN :" << book[i].ISBNNumber << endl;
+//                 cout << "AuthorName :" << book[i].authorName << endl;
+//                 cout << "PublisherName :" << book[i].publisherName << endl;
+//                 found = 1;
+//                 break;
+//             }
+
+//         }
+//         if (!found){
+//             cout << "Book not found." << endl;
+//         }
+//     };
+
+// };
+
+// int main(){
+//     Book book[5];
+//     string options[5] = {"Add Book.", "Remove Book.", "Search Book.", "See Book Info.", "Exit."};
+//     int tempOption;
+
+//     while (tempOption != 5){
+//         for (int i = 0; i < 5; ++i){
+//             cout << i + 1 << ". " << options[i] << endl;
+//         }
+    
+//         cout << "Enter your choice..!" << endl;
+//         cin >> tempOption;
+    
+//         if (tempOption == 5){
+//             exit;
+//         }
+//         else if (tempOption == 1) {
+//             book->AddBook(book) ;
+//         }
+//         else if (tempOption == 2) {
+//             book->RemoveBook(book);
+//         }
+//         else if (tempOption == 3) {
+//             book->SearchBook(book);
+//         }
+//         else if (tempOption == 4) {
+//             book->AddBook(book);
+//         }
+//     }
+
+// }
