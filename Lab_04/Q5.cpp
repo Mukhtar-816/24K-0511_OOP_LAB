ee5bd5a362442623c9a6a1781f63fa39a5e1cafd
Mukhtar Shaikh <115702231+Mukhtar-816@users.noreply.github.com>
2025-02-21 19:30:44 +0500

    Add files via upload

--- Lab_04/Q5.cpp
+++ Lab_04/Q5.cpp
@@ -1,143 +1,131 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Book {
+class MenuItem {
+public:
+    string name, type;
+    double price;
+
+    MenuItem(string n, string t, double p) : name(n), type(t), price(p) {}
+};
+
+class CoffeeShop {
 private:
-    string bookName;
-    string isbn;
-    string author;
-    string publisher;
+    string name;
+    vector<MenuItem> menu;
+    vector<string> orders;
 
 public:
-    Book(string bookName, string isbn, string author, string publisher) {
-        this->bookName = bookName;
-        this->isbn = isbn;
-        this->author = author;
-        this->publisher = publisher;
+    CoffeeShop(string n) : name(n) {}
+
+    void addMenuItem(string itemName, string type, double price) {
+        menu.push_back(MenuItem(itemName, type, price));
     }
 
-    string getBookName() { return this->bookName; }
-    string getISBN() { return this->isbn; }
-    string getAuthor() { return this->author; }
-    string getPublisher() { return this->publisher; }
+    string addOrder(string itemName) {
+        for (MenuItem &item : menu) {
+            if (item.name == itemName) {
+                orders.push_back(itemName);
+                return "Order added: " + itemName;
+            }
+        }
+        return "This item is currently unavailable";
+    }
 
-    string getBookInfo() {
-        return "Book Name: " + this->bookName + "\nISBN: " + this->isbn + "\nAuthor: " + this->author + "\nPublisher: " + this->publisher;
+    string fulfillOrder() {
+        if (!orders.empty()) {
+            string item = orders.front();
+            orders.erase(orders.begin());
+            return "The " + item + " is ready";
+        }
+        return "All orders have been fulfilled";
+    }
+
+    vector<string> listOrders() {
+        return orders;
+    }
+
+    double dueAmount() {
+        double total = 0.0;
+        for (string &order : orders) {
+            for (MenuItem &item : menu) {
+                if (item.name == order) {
+                    total += item.price;
+                }
+            }
+        }
+        return total;
+    }
+
+    string cheapestItem() {
+        if (menu.empty()) return "No items in the menu";
+        MenuItem *cheapest = &menu[0];
+        for (MenuItem &item : menu) {
+            if (item.price < cheapest->price) {
+                cheapest = &item;
+            }
+        }
+        return cheapest->name;
+    }
+
+    vector<string> drinksOnly() {
+        vector<string> drinks;
+        for (MenuItem &item : menu) {
+            if (item.type == "drink") {
+                drinks.push_back(item.name);
+            }
+        }
+        return drinks;
+    }
+
+    vector<string> foodOnly() {
+        vector<string> food;
+        for (MenuItem &item : menu) {
+            if (item.type == "food") {
+                food.push_back(item.name);
+            }
+        }
+        return food;
     }
 };
 
 int main() {
-    Book books[5] = {
-        Book("The Catcher in the Rye", "978-0-316-76948-0", "J.D. Salinger", "Little, Brown and Company"),
-        Book("1984", "978-0-452-28423-4", "George Orwell", "Secker & Warburg"),
-        Book("To Kill a Mockingbird", "978-0-06-112008-4", "Harper Lee", "J.B. Lippincott & Co."),
-        Book("The Great Gatsby", "978-0-7432-7356-5", "F. Scott Fitzgerald", "Charles Scribner's Sons"),
-        Book("Moby Dick", "978-0-14-243724-7", "Herman Melville", "Harper & Brothers")
-    };
+    CoffeeShop shop("Cafe Delight");
 
-    for (int i = 0; i < 5; i++) {
-        cout << "Book " << i + 1 << " Info:\n" << books[i].getBookInfo() << "\n\n";
+    shop.addMenuItem("Espresso", "drink", 2.50);
+    shop.addMenuItem("Latte", "drink", 3.00);
+    shop.addMenuItem("Croissant", "food", 2.75);
+    shop.addMenuItem("Muffin", "food", 2.25);
+
+    cout << shop.addOrder("Espresso") << endl;
+    cout << shop.addOrder("Muffin") << endl;
+    cout << shop.addOrder("Burger") << endl;
+
+    cout << "Orders: ";
+    for (string &order : shop.listOrders()) {
+        cout << order << ", ";
     }
+    cout << endl;
+
+    cout << "Due amount: $" << shop.dueAmount() << endl;
+    cout << shop.fulfillOrder() << endl;
+    cout << shop.fulfillOrder() << endl;
+    cout << shop.fulfillOrder() << endl;
+
+    cout << "Cheapest item: " << shop.cheapestItem() << endl;
+
+    cout << "Drinks only: ";
+    for (string &drink : shop.drinksOnly()) {
+        cout << drink << ", ";
+    }
+    cout << endl;
+
+    cout << "Food only: ";
+    for (string &food : shop.foodOnly()) {
+        cout << food << ", ";
+    }
+    cout << endl;
 
     return 0;
 }
-
-
-
-
-
-
-// using namespace std;
-// #include <string.h>
-
-// class Book {
-//     public :
-//     string bookName;
-//     string ISBNNumber;
-//     string authorName;
-//     string publisherName;
-//     int index = 0;
-
-
-//     void AddBook(Book *book){
-//         cout << "Enter BookName, ISBN number, AuthorName, and PublisherName respectively :" << endl;
-//         cin >> book[index].bookName >> book[index].ISBNNumber >> book[index].authorName >> book[index].publisherName;
-//         index++;
-//         cout << "Book Added." << endl;
-//     }
-
-//     void RemoveBook(Book *book){
-//         string tempID; int found = 0;
-//         cout << "Enter Book ISBN Number to remove :" << endl;
-//         cin >> tempID;
-
-//         for (int i = 0; i < 5; ++i){
-//             if (tempID == book[i].ISBNNumber){
-//                 book[i].bookName = "";
-//                 book[i].ISBNNumber = "";
-//                 book[i].authorName = "";
-//                 book[i].publisherName = "";
-//                 found = 1;
-//                 cout << "Book Removed." << endl;
-//                 break;
-//             }
-//         }
-//         if (!found){
-//             cout << "Book not found." << endl;
-//         }
-//     };
-
-//     void SearchBook(Book *book){
-//         string tempID; int found = 0;
-//         cout << "Enter Book ISBN Number :" << endl;
-//         cin >> tempID;
-
-//         for (int i = 0; i < 5; ++i){
-//             if (tempID == book[i].ISBNNumber){
-//                 cout << "BookName :" << book[i].bookName << endl;
-//                 cout << "ISBN :" << book[i].ISBNNumber << endl;
-//                 cout << "AuthorName :" << book[i].authorName << endl;
-//                 cout << "PublisherName :" << book[i].publisherName << endl;
-//                 found = 1;
-//                 break;
-//             }
-
-//         }
-//         if (!found){
-//             cout << "Book not found." << endl;
-//         }
-//     };
-
-// };
-
-// int main(){
-//     Book book[5];
-//     string options[5] = {"Add Book.", "Remove Book.", "Search Book.", "See Book Info.", "Exit."};
-//     int tempOption;
-
-//     while (tempOption != 5){
-//         for (int i = 0; i < 5; ++i){
-//             cout << i + 1 << ". " << options[i] << endl;
-//         }
-    
-//         cout << "Enter your choice..!" << endl;
-//         cin >> tempOption;
-    
-//         if (tempOption == 5){
-//             exit;
-//         }
-//         else if (tempOption == 1) {
-//             book->AddBook(book) ;
-//         }
-//         else if (tempOption == 2) {
-//             book->RemoveBook(book);
-//         }
-//         else if (tempOption == 3) {
-//             book->SearchBook(book);
-//         }
-//         else if (tempOption == 4) {
-//             book->AddBook(book);
-//         }
-//     }
-
-// }
